1cc476b9abe9995755523ca9f109de5831ee3c79
Redekian <redekiangaming@gmail.com>
2021-09-27 21:39:40 +0300

    ping/pong command added

--- index.ts
+++ index.ts
@@ -2,11 +2,19 @@ import Discord, { Intents } from "discord.js";
 import { token } from "./config.json";
 
 const client: Discord.Client = new Discord.Client({
-  intents: [Intents.FLAGS.GUILDS],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.once("ready", () => {
-  console.log("Bot is ready to rumble");
+  console.log("Bot is ready");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content == "!ping") {
+    message.reply({
+      content: "Pong!",
+    });
+  }
 });
 
 client.login(token);
